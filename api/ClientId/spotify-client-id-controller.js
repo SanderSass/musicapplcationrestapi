f68b6870d70467f3b302c6f5b0f6705217a155a1
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2022-02-07 19:22:00 +0100

    Update

--- api/ClientId/spotify-client-id-controller.js
+++ api/ClientId/spotify-client-id-controller.js
@@ -4,11 +4,11 @@ module.exports = {
     postClientID: (req, res) => {
         const body = req.body;
         create(body, (err, results) => {
-            if(!err) {
+            if(err) {
                 console.log(err);
                 return res.status(500).json({
                     success: 0,
-                    message: "Database connection error!"
+                    message: err.code
                 });
             } else {
                 return res.status(300).json({
