f8e40273676cd9d5eb8548d60c079c72c0c45f7e
Sander Siimann <58694306+SanderSass@users.noreply.github.com>
2022-03-07 16:37:18 +0100

    json update

--- schemas/clientid.js
+++ schemas/clientid.js
@@ -1,21 +1,25 @@
 const ajvInstance = require('./ajv-instance');
 
 const clientID = {
+  $schema: "http://json-schema.org/draft-07/schema#",
   type: "object",
   title: "Spotify Client IDs",
   description: "This document records the details of an user spotify client IDs",
   properties: {
     UserID: {
         title: "UserID", 
-        type: "string"
+        type: "string",
+        minLength: 3
     },
     ClientID: {
       title: "ClientID", 
-      type: "string"
+      type: "string",
+      minLength: 3
     },
     iv: {
       title: "iv", 
-      type: "string"
+      type: "string",
+      minLength: 1
     }
   },
   required: [		
